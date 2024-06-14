9f65472fb9cab4255d88aefa861999f922738d85
Sudipta Das <shiningsudipto@gmail.com>
2024-06-14 18:37:46 +0600

    booking modal and interface added

---
+++ src/app/modules/booking/booking.interface.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/consistent-type-definitions */
+// src/interfaces/booking.interface.ts
+
+import { Types } from 'mongoose'
+
+export type VehicleType =
+  | 'car'
+  | 'truck'
+  | 'SUV'
+  | 'van'
+  | 'motorcycle'
+  | 'bus'
+  | 'electricVehicle'
+  | 'hybridVehicle'
+  | 'bicycle'
+  | 'tractor'
+
+export interface TBooking {
+  customer: Types.ObjectId
+  service: Types.ObjectId
+  slot: Types.ObjectId
+  vehicleType: VehicleType
+  vehicleBrand: string
+  vehicleModel: string
+  manufacturingYear: number
+  registrationPlate: string
+}
---
+++ src/app/modules/booking/booking.service.ts
@@ -0,0 +1,7 @@
+import express from 'express'
+
+const router = express.Router()
+// service creating route
+router.post('/')
+
+export const ServiceRoutes = router
