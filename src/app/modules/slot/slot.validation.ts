96f76f3fa4a57510f7350e0d251c45c1d7f020bb
Sudipta Das <shiningsudipto@gmail.com>
2024-06-15 12:41:23 +0600

    slot creation validation added

---
+++ src/app/modules/slot/slot.validation.ts
@@ -0,0 +1,40 @@
+import { z } from 'zod'
+
+const objectIdPattern = /^[0-9a-fA-F]{24}$/ // Pattern for validating MongoDB ObjectId
+
+const createSlotValidationSchema = z.object({
+  body: z.object({
+    service: z
+      .string({
+        invalid_type_error: 'Service ID must be a string',
+      })
+      .refine((value) => objectIdPattern.test(value), {
+        message: 'Invalid Service ID format',
+      }),
+    date: z
+      .string({
+        invalid_type_error: 'Date must be a string',
+      })
+      .refine((value) => !isNaN(Date.parse(value)), {
+        message: 'Invalid date format',
+      }),
+    startTime: z
+      .string({
+        invalid_type_error: 'Start time must be a string',
+      })
+      .regex(/^([01]\d|2[0-3]):?([0-5]\d)$/, 'Invalid start time format'),
+    endTime: z
+      .string({
+        invalid_type_error: 'End time must be a string',
+      })
+      .regex(/^([01]\d|2[0-3]):?([0-5]\d)$/, 'Invalid end time format'),
+    isBooked: z.enum(['booked', 'available', 'canceled'], {
+      invalid_type_error:
+        'isBooked must be one of "booked", "available", "canceled"',
+    }),
+  }),
+})
+
+export const SlotValidations = {
+  createSlotValidationSchema,
+}
