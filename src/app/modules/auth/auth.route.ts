7afee4652a234e439b7eeecb4fbf7cba3fab1fe0
Sudipta Das <shiningsudipto@gmail.com>
2024-06-15 12:17:55 +0600

    login and signup validation added

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -3,6 +3,7 @@ import { AuthControllers } from './auth.controller'
 import validateRequest from '../../middlewares/validateRequest'
 import { AuthValidation } from './auth.validation'
 import { userControllers } from '../user/user.controller'
+import { UserValidations } from '../user/user.validation'
 
 const router = express.Router()
 
@@ -11,6 +12,10 @@ router.post(
   validateRequest(AuthValidation.loginValidationSchema),
   AuthControllers.loginUser,
 )
-router.post('/signup', userControllers.createUser)
+router.post(
+  '/signup',
+  validateRequest(UserValidations.createUserValidationSchema),
+  userControllers.createUser,
+)
 
 export const AuthRoutes = router
